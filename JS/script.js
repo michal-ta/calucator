41a2f4ea3191e53bce4fe372bc38d1a87387197f
Michal Takáč <michaltakac93@gmail.com>
2022-03-07 20:22:33 +0100

    changin of theme , adding variables in .css file, adding changing into JS

--- JS/script.js
+++ JS/script.js
@@ -4,21 +4,34 @@ let buttons=document.querySelector('button')
 let themeNormal=document.querySelector('.slider1')
 let themeDarker=document.querySelector('.slider2')
 let themePurple=document.querySelector('.slider3')
+let themeManipulation=document.getElementById('changing');
+
+console.log(themeManipulation)
+
 
 function theme1(){
     themeNormal.classList.add('active');
     themeDarker.classList.remove('active');
     themePurple.classList.remove('active');
+    themeManipulation.classList.remove('theme2');
+    themeManipulation.classList.remove('theme3');
+    themeManipulation.classList.add('theme1');
 }
 
 function theme2(){
     themeNormal.classList.remove('active');
     themeDarker.classList.add('active');
     themePurple.classList.remove('active');
+    themeManipulation.classList.add('theme2')
+    themeManipulation.classList.remove('theme3')
+    themeManipulation.classList.remove('theme1')
 }
 
 function theme3(){
     themeNormal.classList.remove('active');
     themeDarker.classList.remove('active');
     themePurple.classList.add('active');
+    themeManipulation.classList.remove('theme2');
+    themeManipulation.classList.add('theme3');
+    themeManipulation.classList.remove('theme1');
 }
